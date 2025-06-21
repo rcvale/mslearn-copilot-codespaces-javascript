b8ac3e63ca1b8e2cb1eae06ee0eea3600ad95f58
Ricardo Cunha Vale <ricardovalec@gmail.com>
2025-06-21 20:37:42 +0000

    Atualiza informações do siteProps e adiciona animação aos ícones sociais

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Ricardo",
+  title: "Desenvolvedor",
+  email: "Gmail",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "microsoft",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
